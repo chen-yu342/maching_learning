c672ae0103bf09a53792d36469ee244eb91d3569
18811 <18811392166@163.com>
2023-11-21 23:35:14 +0800

    logRegres

---
+++ logistic/logRegres.py
@@ -0,0 +1,19 @@
+
+
+
+def loadDataSet():
+    dataMat = [];labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0,float(lineArr[0]),float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat,labelMat
+
+
+
+
+
+
+if __name__ == '__main__':
+    loadDataSet()
